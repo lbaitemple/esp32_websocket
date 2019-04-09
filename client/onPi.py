90f99fe70a7c1df90650d07d94be974206bb3e5a
lbaitemple <lbai@temple.edu>
2019-04-09 07:54:58 -0400

    Update onPi.py

--- client/onPi.py
+++ client/onPi.py
@@ -8,6 +8,6 @@ dict = {}  # Store data in dict
 dict['method'] = 'PWM'  # Poll temperature sensor
 dict['Pin'] = 12  # Read ESP32 internal temp
 dict['freq'] = 100  # Recor
-dict['duty']=sys.argv[1]
+dict['duty']=0.15
 
 ws.send(json.dumps(dict))
